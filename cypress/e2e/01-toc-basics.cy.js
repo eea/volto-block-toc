c3521541de52974be1d3a72c646b0e70022077f1
dobri1408 <dobriceanionut1408@gmail.com>
2024-07-04 10:02:49 +0300

    Add cypress test

--- cypress/e2e/01-toc-basics.cy.js
+++ cypress/e2e/01-toc-basics.cy.js
@@ -194,4 +194,54 @@ describe('Block Tests: Toc', () => {
     cy.contains('Volto Toc');
     cy.get('.table-of-contents  .dropdown').contains('More').click();
   });
+
+  it('Add Block: add side menu TOC', () => {
+    // Change page title
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('Volto Toc');
+    cy.getSlate().click();
+
+    // Add TOC block
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get(".blocks-chooser .ui.form .field.searchbox input[type='text']").type(
+      'table of contents',
+    );
+    cy.get('.button.toc').click();
+
+    cy.get('#sidebar-properties .form .react-select-container').first().click();
+    cy.contains('Side Menu').click();
+
+    // Add headings
+    cy.get('.ui.drag.block.inner.slate').click().type('Title 1').click();
+    cy.get('.ui.drag.block.inner.slate span span span').setSelection('Title 1');
+    cy.get('.slate-inline-toolbar .button-wrapper a[title="Title"]').click({
+      force: true,
+    });
+    cy.get('.ui.drag.block.inner.slate').click().type('{enter}');
+
+    cy.get('.ui.drag.block.inner.slate').eq(1).click().type('Title 2').click();
+    cy.get('.ui.drag.block.inner.slate span span span')
+      .eq(1)
+      .setSelection('Title 2');
+    cy.get('.slate-inline-toolbar .button-wrapper a[title="Title"]').click({
+      force: true,
+    });
+    cy.get('.ui.drag.block.inner.slate').eq(1).click().type('{enter}');
+
+    // Save page
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+
+    // Check if the page contains the TOC and the headings
+    cy.contains('Volto Toc');
+    cy.contains('Title 1');
+    cy.contains('Title 2');
+    cy.get('a[href="#title-1"]').click();
+    cy.get('a[href="#title-2"]').click();
+    cy.get('h2[id="title-1"]').contains('Title 1');
+    cy.get('h2[id="title-2"]').contains('Title 2');
+    cy.get('.table-of-contents').get('.active.title').click();
+    cy.get('.table-of-contents');
+    cy.get('.table-of-contents .content').should('have.css', 'display', 'none');
+  });
 });
