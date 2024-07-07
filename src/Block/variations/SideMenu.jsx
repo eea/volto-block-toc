ad8ce259a25145a89880799787ca1141598e6664
dobri1408 <dobriceanionut1408@gmail.com>
2024-07-08 00:09:43 +0300

    improve performance by rerendering the state just when menu is too close to footer

--- src/Block/variations/SideMenu.jsx
+++ src/Block/variations/SideMenu.jsx
@@ -5,37 +5,36 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Accordion, Icon } from 'semantic-ui-react';
 import Slugger from 'github-slugger';
 import { normalizeString } from './helpers';
+import { debounce } from 'lodash';
 import './less/side-menu.less';
 
-const RenderMenuItems = ({ items }) => {
-  return (
-    <Accordion fluid styled>
-      {items.map((item, index) => {
-        const { title, override_toc, plaintext, items: subItems } = item;
-        const slug = override_toc
-          ? Slugger.slug(normalizeString(plaintext))
-          : Slugger.slug(normalizeString(title));
-        return (
-          <React.Fragment key={index}>
-            <div className="title">
-              <AnchorLink href={`#${slug}`}>{title}</AnchorLink>
-            </div>
-            {subItems && subItems.length > 0 && (
-              <RenderMenuItems items={subItems} />
-            )}
-          </React.Fragment>
-        );
-      })}
-    </Accordion>
-  );
-};
+const RenderMenuItems = ({ items }) => (
+  <Accordion fluid styled>
+    {items.map((item, index) => {
+      const { title, override_toc, plaintext, items: subItems } = item;
+      const slug = override_toc
+        ? Slugger.slug(normalizeString(plaintext))
+        : Slugger.slug(normalizeString(title));
+      return (
+        <React.Fragment key={index}>
+          <div className="title">
+            <AnchorLink href={`#${slug}`}>{title}</AnchorLink>
+          </div>
+          {subItems && subItems.length > 0 && (
+            <RenderMenuItems items={subItems} />
+          )}
+        </React.Fragment>
+      );
+    })}
+  </Accordion>
+);
 
 const View = (props) => {
   const { data, tocEntries, mode } = props;
   const [open, setOpen] = useState(true);
   const [rendered, setRendered] = useState(false);
 
-  const offset = 120; // minimum distance from fotter
+  const offset = 50; // minimum distance from footer
 
   useEffect(() => {
     if (!rendered) {
@@ -60,73 +59,54 @@ const View = (props) => {
       if (sideMenu && footer && pageDocument) {
         const footerRect = footer.getBoundingClientRect();
         const sideMenuRect = sideMenu.getBoundingClientRect();
-        const pageDocumentRect = pageDocument.getBoundingClientRect();
-        sideMenu.style.position = 'absolute';
-        sideMenu.style.right = 100 + 'px';
-        sideMenu.style.top =
-          Math.max(
-            window.scrollY + pageDocumentRect.top - 20,
-            window.scrollY + 20,
-          ) + 'px';
 
         const distanceToFooter =
-          footerRect.top - sideMenu?.getBoundingClientRect().bottom;
+          footerRect.top - (window.scrollY + sideMenuRect.height);
 
-        //menu is too close from footer
         if (distanceToFooter <= offset) {
-          const newTop = Math.max(
-            window.scrollY + pageDocumentRect.top - 50,
-            window.scrollY + footerRect.top - sideMenuRect.height - offset,
-          );
+          const newTop =
+            window.scrollY + footerRect.top - sideMenuRect.height - offset;
+
           sideMenu.style.position = 'absolute';
           sideMenu.style.top = `${newTop}px`;
         } else {
           //calculate position based on the scroll and start of the page
-          sideMenu.style.top =
-            Math.max(
-              window.scrollY + pageDocumentRect.top - 20,
-              window.scrollY + 20,
-            ) + 'px';
+          sideMenu.style.position = 'fixed';
+          sideMenu.style.top = 'unset';
         }
       }
     }
 
-    window.addEventListener('scroll', adjustSideMenuPosition);
+    window.addEventListener('scroll', debounce(adjustSideMenuPosition, 100));
     adjustSideMenuPosition();
 
     return () => {
-      window.removeEventListener('scroll', adjustSideMenuPosition);
+      window.removeEventListener(
+        'scroll',
+        debounce(adjustSideMenuPosition, 250),
+      );
     };
   }, [rendered, mode, open, data.variation, props.device]);
 
-  if (tocEntries?.length > 0)
-    return (
-      <Accordion fluid styled>
-        <React.Fragment index={0}>
-          <Accordion.Title
-            active={open}
-            onClick={() => {
-              setOpen(!open);
-            }}
-          >
-            <Icon
-              name={open ? 'angle up' : 'angle right'}
-              className="menuTitle"
-            />
+  if (!tocEntries?.length) return null;
 
-            <p className="menuTitle">{data?.title || ''}</p>
-          </Accordion.Title>
-          <Accordion.Content active={open}>
-            <RenderMenuItems items={tocEntries} />
-          </Accordion.Content>
-        </React.Fragment>
-      </Accordion>
-    );
-  else return <></>;
+  return (
+    <Accordion fluid styled>
+      <Accordion.Title active={open} onClick={() => setOpen(!open)}>
+        <Icon name={open ? 'angle up' : 'angle right'} className="menuTitle" />
+        <p className="menuTitle">{data?.title || ''}</p>
+      </Accordion.Title>
+      <Accordion.Content active={open}>
+        <RenderMenuItems items={tocEntries} />
+      </Accordion.Content>
+    </Accordion>
+  );
 };
 
 View.propTypes = {
-  properties: PropTypes.objectOf(PropTypes.any).isRequired,
+  data: PropTypes.object.isRequired,
+  tocEntries: PropTypes.array,
+  mode: PropTypes.string,
 };
 
 export default injectIntl(View);
